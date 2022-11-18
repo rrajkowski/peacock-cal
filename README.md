3913b9ab7c84c964b4555cdf8dec0280f1f77f91
rrajkowski <ruben.rajkowski@gmail.com>
2022-11-17 23:38:58 -0700

    updates to ics, new libs

--- README.md
+++ README.md
@@ -7,11 +7,13 @@ https://peacock-cal.herokuapp.com/v1/send
 
 #### Params
 
-- `title`:  Event title, Email subject
-- `start`: DateTime (utc iso 8601)
-- `end`: DateTime (utc iso 8601)
-- `url`: link to event
-- `attendees`: (email address array)
+- `title (string)`:  Event title, Email subject
+- `start (datetime)`: UTC ISO 8601
+- `end (datetime)`: UTC ISO 8601
+- `url (string)`: link to event
+- `body (string)`: Email body
+- `location (string)`: Event Address
+- `attendees (array)`: Email address
 
 
 http://localhost:3000/v1/ics
@@ -24,4 +26,14 @@ https://peacock-cal.herokuapp.com/v1/ics
 - `start`: DateTime (utc iso 8601)
 - `end`: DateTime (utc iso 8601)
 - `url`: link to event (location)
+
+
+#### Start app
+
+`yarn dev` => local
+`yarn start` => prod
+
+
+#### Publish to Heroku
+`git push heroku main`
 
--- src/routes/ics.ts
+++ src/routes/ics.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import Route from '../interfaces/routes';
-
+import { writeFileSync } from 'fs';
+import * as moment from 'moment';
+import * as ics from 'ics';
+import HttpException from '../exceptions/HttpException';
 /**
  * @swagger
  *
@@ -23,11 +26,66 @@ class ICSRoute implements Route {
 		this.initializeRoutes();
 	}
 
-	private initializeRoutes() {
-		this.router.get('/', (req, res) => {
-			res.json({ ics: "success" });
+  /* Create .ics file and return, use correct type/mime */
+  private createICSFile = async (req: any) => {
+
+		// set event for ics
+		let event: any = {
+			title: 'Reminder to watch PeacockTV',
+			description: 'Reminder',
+			busyStatus: 'FREE',
+			start: [2022, 11, 17, 18, 0],
+			end: [2022, 11, 17, 20, 30],
+			location: 'PeacockTV',
+			url: 'https://peacocktv.com',
+			attendees: [],
+		}
+
+		const now = moment().utc();
+		const title = req.query.title || '';
+		const location = req.query.location || '';
+		const url = req.query.url || '';
+		const start = req.query.start || now.toISOString();
+		const end = req.query.end || now.add(2, 'hours').toISOString();
+		//force array, if only 1
+		const attendees = req.query.attendees.length===1 ? [req.query.attendees] : req.query.attendees.split(',');
+
+		// set event params
+		event.title = title;
+		event.start = moment(start).format('YYYY-M-D-H-m').split("-").map(Number);
+		event.end = moment(end).format("YYYY-M-D-H-m").split("-").map(Number);
+		event.url = url.toString().trim();
+		event.location = location.toString().trim();
+		// loop through attendees
+		attendees.forEach((email: string) => {
+			event.attendees.push({ name: email.split('@')[0], email: email, rsvp: true, partstat: 'ACCEPTED', role: 'REQ-PARTICIPANT' });
 		});
-	}
+		let icsFile: any;
+		let icsBinary: any;
+		ics.createEvent(event, (err, value) => {
+			if (err) {
+				throw new HttpException(404, 'ICS createEvent failed: '+ JSON.stringify(err));
+			}
+			if(value){
+				icsBinary = value;
+				icsFile =	writeFileSync(`${__dirname}/../../files/event.ics`, value);
+			}
+		});
+
+
+    // return base64 .ics file
+    return await icsBinary.toString('base64');
+  }
+
+	/* All Routes */
+  private initializeRoutes = async() => {
+    await this.router.get('/', async(req, res) => {
+			res.set('content-type', 'text/calendar; charset=utf-8');
+			res.set('content-disposition', 'inline; filename=calendar.ics');
+			res.type('ics');
+      res.status(200).send(await this.createICSFile(req));
+    });
+  }
 }
 
 export default ICSRoute;
